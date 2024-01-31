0ae07eeec3c063f5b23be6bc07408e4ca33e1a23
renan-fs-sousa <renan.fs.sousa@gmail.com>
2024-01-31 10:39:18 -0300

    ajuste indentação

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const alunos = [
     { nome: "Lucas", nota: 6 },
     { nome: "Lucia", nota: 8 },
     { nome: "Marcos", nota: 2 }
-    ];
+];
 
 function filtrarAprovados(arrayAlunos) {
     return arrayAlunos.filter(aluno => aluno.nota >= 6);
-    }
+}
 
 const alunosAprovados = filtrarAprovados(alunos);
 

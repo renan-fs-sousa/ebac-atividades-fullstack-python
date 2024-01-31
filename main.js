59781df1ba71fa505b9a8704f30dc33b6feb13f3
renan-fs-sousa <renan.fs.sousa@gmail.com>
2024-01-31 10:33:04 -0300

    atividade es6

---
+++ main.js
@@ -0,0 +1,15 @@
+const alunos = [
+    { nome: "Renan", nota: 10 },
+    { nome: "Maria", nota: 3 },
+    { nome: "Lucas", nota: 6 },
+    { nome: "Lucia", nota: 8 },
+    { nome: "Marcos", nota: 2 }
+    ];
+
+function filtrarAprovados(arrayAlunos) {
+    return arrayAlunos.filter(aluno => aluno.nota >= 6);
+    }
+
+const alunosAprovados = filtrarAprovados(alunos);
+
+console.log(alunosAprovados);
